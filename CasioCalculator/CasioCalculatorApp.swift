563c8c3b692d806e9f97dffe1d77e1fffb1ca7a8
Jordan Mck <ghostdagunna@gmail.com>
2023-09-12 15:26:53 -0500

    Initial Commit

---
+++ CasioCalculator/CasioCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasioCalculatorApp.swift
+//  CasioCalculator
+//
+//  Created by Jordan Mck on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CasioCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
